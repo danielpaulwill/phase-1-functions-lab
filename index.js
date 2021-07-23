d8c9fb95510513f8f7c62d4357de0c9fdc8f883e
Daniel Williams <danielpaulwill@gmail.com>
2021-07-23 13:11:56 -0400

    commit tree aa328641cf72f208e385e0028df4f353e1f60c17

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+const HQ = 42
+
+function distanceFromHqInBlocks(destinationBlock) {
+    if (destinationBlock >= HQ){
+        return parseInt(destinationBlock - HQ);
+    }
+    else return parseInt(HQ - destinationBlock);
+}
+
+function distanceFromHqInFeet(destinationBlock) {
+    let result = distanceFromHqInBlocks(destinationBlock);
+    return result * 264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    if (startBlock <= endBlock) {
+        return parseInt(endBlock - startBlock) * 264;
+    }
+    else return parseInt(startBlock - endBlock) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let result = distanceTravelledInFeet(start, destination);
+    if (result <= 400)
+        return 0;
+    else if (result > 2500)
+        return "cannot travel that far"
+    else if (result > 2000)
+        return 25;
+    else (result > 400)
+        return (result - 400) * .02;
+    
+    
+
+}
+
+
+
+
+console.log(calculatesFarePrice(34, 32));
